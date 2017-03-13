da4e92dbd1a850dd6d890588b48794e57019d1ea
Ian Williams <me@ianwillia.ms>
2017-03-13 08:34:13 -0400

    Index page.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var serve = require('gulp-serve');
+var rollup = require('gulp-rollup');
+
+//
+// === Tasks ===
+//
  
 gulp.task('serve', serve(['build']));
 
@@ -15,46 +20,65 @@ gulp.task('html', function() {
 		.pipe(gulp.dest('build'));
 });
 
+gulp.task('assets', function() {
+	return gulp.src('./public/assets/**/*')
+		.pipe(gulp.dest('./build/assets'));
+});
+
 gulp.task('svg', function() {
-	return gulp.src('public/**/*.svg')
+	return gulp.src('public/images/**/*.svg')
 		.pipe(htmlmin({collapseWhitespace: true}))
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest('build/images'));
 });
 
 gulp.task('js', () => {
-	return gulp.src('src/index.js')
+	return gulp.src('./src/**/*.js')
+		.pipe(rollup({
+			entry: './src/index.js'
+		}))
 		.pipe(babel())
 		.pipe(uglify())
+		.on('error', console.log)
 		.pipe(gulp.dest('./build/assets'));
 });
  
-gulp.task('sass', function () {
+gulp.task('sass', function() {
 	return gulp.src('./src/styles/index.scss')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(gulp.dest('./build/assets'));
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('./build/assets'));
 });
 
-gulp.task('html:watch', function () {
+//
+// === Watch ===
+//
+
+gulp.task('html:watch', function() {
 	gulp.watch('./public/**/*.html', ['html']);
 });
 
-gulp.task('svg:watch', function () {
-	gulp.watch('./public/**/*.svg', ['svg']);
+gulp.task('assets:watch', function() {
+	gulp.watch('./public/assets/**/*', ['assets']);
 });
 
-gulp.task('js:watch', function () {
+gulp.task('svg:watch', function() {
+	gulp.watch('./public/images/**/*.svg', ['svg']);
+});
+
+gulp.task('js:watch', function() {
 	gulp.watch('./src/**/*.js', ['js']);
 });
  
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', function() {
 	gulp.watch('./src/**/*.scss', ['sass']);
 });
+
+// Public Tasks
 
 gulp.task('deploy', function() {
 	return gulp.src(['./build/**/*', './CNAME'])
 	.pipe(ghPages());
 });
 
-gulp.task('default', ['html', 'svg', 'js', 'sass']);
-gulp.task('watch', ['serve', 'js:watch', 'sass:watch', 'html:watch', 'svg:watch',]);
+gulp.task('default', ['html', 'svg', 'js', 'sass', 'assets']);
+gulp.task('watch', ['serve', 'js:watch', 'sass:watch', 'html:watch', 'svg:watch', 'assets:watch',]);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-console.log('yo');
-
-
-console.log('yo');
-
-console.log('yo');
-
-console.log('yo');
+console.log(window.swiper);
